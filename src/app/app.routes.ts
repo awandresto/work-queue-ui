e48fc4ee389ce15aa5a38ba37927caade5bd1073
Andrii Pivovarov <awandresto@gmail.com>
2025-05-27 17:59:07 +0300

    TK-02 - Created Dashboard component and implemented routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    component: DashboardComponent,
+    path: 'dashboard'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  }
+];
