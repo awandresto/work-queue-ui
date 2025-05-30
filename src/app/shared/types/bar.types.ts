b19819de5ed2d2e64feb4c641e4ef17bcae8fe7c
Andrii Pivovarov <awandresto@gmail.com>
2025-05-30 15:56:58 +0300

    TK-02 - Created shared component for a target progress bar

---
+++ src/app/shared/types/bar.types.ts
@@ -0,0 +1,24 @@
+export interface GoalBarSegment {
+  width: number;
+  color: string;
+}
+
+export interface GoalBarData {
+  bottomLabel: string;
+  segments: GoalBarSegment[];
+  showArrowBottom: boolean;
+  showArrowTop: boolean;
+  title: string;
+  topArrowPosition: number;
+  topLabel: string;
+  valueLabel: string;
+  valuePosition: number;
+}
+
+export interface TargetBarData {
+  percent: number;
+  percentLabel: string;
+  target: string;
+  title: string;
+  value: string;
+}
